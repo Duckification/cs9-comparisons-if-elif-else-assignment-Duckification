7da5425c2d43a29278f8ffe000e31f4b732d811e
Duckification <94643799+Duckification@users.noreply.github.com>
2021-12-09 00:01:34 -0500

    Update part4.py

--- part4.py
+++ part4.py
@@ -1,26 +1,14 @@
-'''
-______
-PART 4
-______
-Write a program that prompts the user to enter two integer inputs. The program prints a string stating if the inputs are both positive, negative, if one of the inputs is zero, or if they have opposite signs. (Hint: this code can be written using only four code blocks, but you may find ways to do this using more.)
-
-Four examples of what should appear on the console when this program runs (note: the test driver is case sensitive):
-
-Enter a number:  3
-Enter another number:  7
-positive
-
-Enter a number:  -5
-Enter another number:  -2
-negative
-
-Enter a number:  7
-Enter another number:  0
-zero
-
-Enter a number:  2
-Enter another number:  -2
-opposite
-'''
-
-#start writing your code below
+num1 = int(input("Enter a number: "))
+num2 = int(input("Enter another number: "))
+if(num1 == 0):
+  print("zero")
+elif(num2 == 0):
+  print("zero")
+elif(num1 > 0 and num2 > 0):
+  print("positive")
+elif(num1 <= 0 and num2 <= 0):
+  print("negative")
+elif(num1 > 0 and num2 <= 0):
+  print("opposite")
+elif(num1 <= 0 and num2 > 0):
+  print("opposite")
