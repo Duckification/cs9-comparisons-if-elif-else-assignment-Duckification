4fe65acaf17d88633d0958f1c242062608e067fa
Duckification <94643799+Duckification@users.noreply.github.com>
2021-12-09 00:01:51 -0500

    Update part5.py

--- part5.py
+++ part5.py
@@ -1,25 +1,10 @@
-'''
-______
-PART 5
-______
+month = input("Enter a month: ")
 
-Write a program that asks the user to enter the name of a month as a string. The program will then print how many days that month could have in any given year, or print a statement saying that what the user entered is not the name of a month.
-
-(Hint: This should require only four code blocks, but it can be done with 12 or more if you insist. If you are familiar with lists or other container datatypes, you may implement this using those, though it still requires four code blocks)
-
-Four examples of what should appear on the console when the program runs (note: the test driver is case sensitive):
-
-Enter a month:  March
-31
-
-Enter a month:  June
-30
-
-Enter a month:  February
-28 or 29
-
-Enter a month:  Saturday
-not a month
-'''
-
-#start writing your code below
+if(month == "January" or month == "january" or month == "March" or month == "march" or month == "may" or month == "May" or month == "July" or month == "july" or month == "August" or month == "august" or month == "october" or month == "October" or month == "December" or month == "december"):
+  print("31")
+elif(month == "February" or month == "february"):
+  print("28")
+elif(month == "April" or month == "april" or month == "June" or month == "june" or month == "september" or month == "September" or month == "november" or month == "November"):
+  print("30")
+else:
+  print ("learn yo months ")
