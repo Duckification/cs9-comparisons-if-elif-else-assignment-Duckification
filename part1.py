d2dd7ace7e93ffe818c458666951ccfc4e0573e3
Duckification <94643799+Duckification@users.noreply.github.com>
2021-12-09 00:00:21 -0500

    Update part1.py

--- part1.py
+++ part1.py
@@ -1,20 +1,3 @@
-'''
-______
-PART 1
-______
-The code below prompts the user to enter two numbers, and then prints out the smaller of the numbers entered. Modify so that it prompts the user to enter THREE numbers, and then prints the smallest of the three numbers entered in a sentence.
-
-(Hint: You'll need to be careful for the cases when the user enters the same number twice or all three times.)
-
-An example of what should appear on the console when the code is run:
-
-Enter a number: 11
-Enter another number: 2
-Enter another number: 5
-
-The smallest number is 2
-'''
-
 number = int(input("Enter a number: "))
 
 smallest = number
@@ -24,4 +7,8 @@ number = int(input("Enter another number: "))
 if number < smallest:
   smallest = number
   
+duck = int(input("Enter another number: "))
+if duck < smallest:
+  smallest = duck
+
 print("The smallest number is ", smallest)
