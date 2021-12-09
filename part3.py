f2eb2f80141c302f1607b99d0ef6cefd610cfacd
Duckification <94643799+Duckification@users.noreply.github.com>
2021-12-09 00:01:15 -0500

    Update part3.py

--- part3.py
+++ part3.py
@@ -1,22 +1,12 @@
-'''
-______
-PART 3
-______
-Write a program that asks the user to input one integer. The program will then print two strings, one stating if it's positive, negative, or zero, and another that states whether or not is is divisible by 3. (Hint: to check divisibility by 3, you will find using the modulus(%) operation very helpful.)
+num1 = int(input("Enter a number: "))
+if(num1 == 0):
+  print("zero")
+elif(num1 > 0):
+  print("positive")
+else:
+  print("negative")
 
-3 examples of what should appear on the console when the program runs (note: the test driver is case sensitive):
-
-Enter a number:  -2
-negative
-not divisible by 3
-
-Enter a number:  0
-zero
-divisible by 3
-
-Enter a number:  5
-positive
-not divisible by 3
-'''
-
-#write your code below
+if(num1%3 == 0):
+  print("it is divisible by three")
+else:
+  print("it is not divisible by three")
